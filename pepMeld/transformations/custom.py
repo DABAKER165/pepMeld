23f93509a2cb7f3a77f6b70099337a7f5715238c
David Baker <dabaker3@gmail.com>
2020-08-18 16:09:00 -0500

    Redoing

--- pepMeld/transformations/custom.py
+++
@@ -1,4 +0,0 @@
-from .utils import *
-class custom:
-    def custom_here(self, transformation):
-        return
--- process_arrays.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Processes the arrays."""
-
-from pepMeld.parse_arguments import get_process_arrays_args
-from pepMeld.run_transforms import run_transforms
-
-if __name__ == '__main__':
-	args = get_process_arrays_args()
-	run_transforms(args)
